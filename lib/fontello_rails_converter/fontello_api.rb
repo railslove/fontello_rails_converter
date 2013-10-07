2b0a80e0ddf8717d095f102ec0338dc0a4c1c949
Jakob Hilden <jakobhilden@gmail.com>
2013-10-07 22:36:01 +0200

    more API

--- lib/fontello_rails_converter/fontello_api.rb
+++ lib/fontello_rails_converter/fontello_api.rb
@@ -7,11 +7,18 @@ module FontelloRailsConverter
 
     def initialize(options)
       @config_file = options[:config_file]
-      @session_id = RestClient.post FONTELLO_HOST, config: File.new(@config_file, 'rb')
+    end
+
+    def session_id
+      @session_id ||= RestClient.post FONTELLO_HOST, config: File.new(@config_file, 'rb')
     end
 
     def session_url
-      "#{FONTELLO_HOST}/#{@session_id}"
+      "#{FONTELLO_HOST}/#{session_id}"
+    end
+
+    def download_zip
+      resp = RestClient.get "#{session_url}/get"
     end
   end
 end
