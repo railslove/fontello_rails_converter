580b1005d240016df78a27f1aae6ee80e64f8e40
Jakob Hilden <jakobhilden@gmail.com>
2013-10-07 22:35:16 +0200

    basic specs

---
+++ spec/fontello_api_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe FontelloRailsConverter::FontelloApi do
+  subject { described_class.new config_file: File.expand_path('../fixtures/config.json', __FILE__) }
+
+  describe '#session_url' do
+    before do
+      subject.should_receive(:session_id).and_return "12345"
+    end
+
+    specify do
+      expect(subject.session_url).to eql 'http://fontello.com/12345'
+    end
+  end
+
+  describe '#download' do
+    specify do
+      pending
+      # subject.download_zip
+    end
+  end
+end
