f597c98d55ee883cde5653a1e76947ae15b7998e
Marco Perrando <marco.perrando@gmail.com>
2017-01-27 18:13:02 +0100

    Fix for Rails 5.

--- lib/fontello_rails_converter/railtie.rb
+++ lib/fontello_rails_converter/railtie.rb
@@ -2,8 +2,10 @@ module FontelloRailsConverter
   class Railtie < Rails::Railtie
 
     initializer "fontello_rails_converter.setup" do |app|
-      app.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
-      app.config.assets.precompile << /\.(?:svg|eot|woff|woff2|ttf)$/
+      if app.config.respond_to? (:assets)
+        app.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+        app.config.assets.precompile << /\.(?:svg|eot|woff|woff2|ttf)$/
+      end
     end
 
   end
