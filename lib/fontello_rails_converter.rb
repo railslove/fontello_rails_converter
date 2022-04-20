0b8642e503f92e2f3c40ee5fbc9fb2904796d23e
Amir Sharif <budgeneration@gmail.com>
2022-04-20 09:40:14 -0700

    Support Rails 7

--- lib/fontello_rails_converter.rb
+++ lib/fontello_rails_converter.rb
@@ -6,5 +6,7 @@ require "fontello_rails_converter/fontello_api"
 require "fontello_rails_converter/cli"
 require "fontello_rails_converter/railtie" if defined?(Rails)
 
+require "active_support/isolated_execution_state" if ActiveSupport::VERSION::MAJOR > 6
+
 module FontelloRailsConverter
 end
