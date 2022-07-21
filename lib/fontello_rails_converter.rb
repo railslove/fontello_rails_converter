c32cd14bcd0a2ca030d4c69dd1416c3fc41c987f
Amir Sharif <budgeneration@gmail.com>
2022-07-21 11:09:43 -0700

    remove major version

--- lib/fontello_rails_converter.rb
+++ lib/fontello_rails_converter.rb
@@ -7,7 +7,5 @@ require "fontello_rails_converter/fontello_api"
 require "fontello_rails_converter/cli"
 require "fontello_rails_converter/railtie" if defined?(Rails)
 
-require "active_support/isolated_execution_state" if ActiveSupport::VERSION::MAJOR > 6
-
 module FontelloRailsConverter
 end
